5d67bc849206a15247b1bc3c03f6f4451dd80cb9
Matt Carter <m@ttcarter.com>
2015-07-22 10:19:41 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+angular-bs-popover
+==================
+AngularJS + Bootstrap popover support.
+
+See the [ng-mfdc](http://momsfriendlydevco.github.io/ng-mfdc) showcase for demos.
---
+++ angular-bs-popover.js
@@ -0,0 +1,33 @@
+angular.module('angular-bs-popover', [])
+.directive('popover', function() {
+	return {
+		scope: {
+			popover: '@',
+			popoverTitle: '@?',
+			popoverPosition: '@?',
+			popoverContainer: '@?',
+			popoverTrigger: '@?',
+			popoverHtml: '@?'
+		},
+		restrict: 'A',
+		link: function($scope, elem) {
+			$scope.$watch('popover + popoverTitle + popoverPosition + popoverContainer + popoverTrigger', function() {
+				var isVisible = $(elem).siblings('.popover').length > 0; // Is the popover already shown?
+				$(elem)
+					.popover('destroy')
+					.popover({
+						content: $scope.popover,
+						title: $scope.popoverTitle,
+						placement: $scope.popoverPosition || 'top',
+						container: $scope.popoverContainer || null,
+						trigger: $scope.popoverTrigger || 'hover',
+						html: $scope.popoverHtml || false,
+						animation: false
+					});
+
+				if (isVisible) // Reshow the popover if we WERE using it before
+					$(elem).popover('show');
+			});
+		}
+	}
+});
